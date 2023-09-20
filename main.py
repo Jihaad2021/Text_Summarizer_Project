fee1dd6b21d2848e9f6a1edfe471de8676cf6834
jihaad2021 <jihaadariefpangestu@gmail.com>
2023-09-21 02:04:54 +0700

    model trainer added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from src.textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from src.textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+
 from src.textSummarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion stage"
@@ -29,6 +31,16 @@ try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
     data_transformation = DataTransformationTrainingPipeline()
     data_transformation.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<\n\nx=======x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Trainer stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    model_trainer = ModelTrainerTrainingPipeline()
+    model_trainer.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<\n\nx=======x")
 except Exception as e:
     logger.exception(e)
--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -1,6 +1,6 @@
 from src.textSummarizer.constants import *
 from src.textSummarizer.utils.common import read_yaml, create_directories
-from src.textSummarizer.entity import (DataIngestionConfig, DataValidationConfig, DataTransformationConfig)
+from src.textSummarizer.entity import (DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -56,3 +56,27 @@ class ConfigurationManager:
         )
         
         return data_transformation_config
+    
+    
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        config = self.config.model_trainer
+        params = self.params.TrainingArguments
+        
+        create_directories([config.root_dir])
+        
+        model_trainer_config = ModelTrainerConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            model_ckpt=config.model_ckpt,
+            num_train_epochs=params.num_train_epochs, 
+            warmup_steps=params.warmup_steps, 
+            per_device_train_batch_size=params.per_device_train_batch_size, 
+            weight_decay=params.weight_decay, 
+            logging_steps=params.logging_steps, 
+            evaluation_strategy=params.evaluation_strategy, 
+            eval_steps=params.eval_steps, 
+            save_steps=params.save_steps, 
+            gradient_accumulation_steps=params.gradient_accumulation_steps 
+        )
+        
+        return model_trainer_config
